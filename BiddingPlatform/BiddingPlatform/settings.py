169209e2031948dc3d9ef3bd584b219927ef25f8
elmoushy <seif778811@gmail.com>
2025-05-30 02:51:32 +0300

    cors setting

--- BiddingPlatform/BiddingPlatform/settings.py
+++ BiddingPlatform/BiddingPlatform/settings.py
@@ -26,8 +26,38 @@ SECRET_KEY = "django-insecure-i6j*i=6ysh5#krohtaf4=0_19or9w4ww%cv42i6_2@*a23%n=p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",  # Vite default port
+    "http://127.0.0.1:5173",  # Vite default port
+    "http://localhost:8080",  # Another common development port
+    "http://127.0.0.1:8080",
+]
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -54,6 +84,7 @@ AUTH_USER_MODEL = "User.User"  # Set the custom user model
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # Add CORS middleware before CommonMiddleware
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
